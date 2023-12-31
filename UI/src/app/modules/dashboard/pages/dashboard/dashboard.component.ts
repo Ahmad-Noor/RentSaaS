fa171e372c2d17600316a39af83b16298579f879
Ahmad Noor <ahmadnooreg@hotmail.com>
2023-12-30 23:26:53 -0600

    updated

---
+++ UI/src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';  
+import { MasterService } from '../../../../core/services/master/master.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent  {
+
+  ticketsArray: any[] = [];
+  selectedProjectData: any ;
+  status: string[]= ['To Do','In Progress','Done'];
+
+  constructor(private master: MasterService, private http: HttpClient) {
+    this.master.onProjectChange.subscribe((res: any) => {
+      debugger;
+      this.getProjectTickets(res.projectId);
+      this.selectedProjectData = res;
+    })
+    this.master.onTicketCreate.subscribe((res: any) => {
+      debugger;
+      this.getProjectTickets(this.selectedProjectData.projectId);
+    })
+  }
+
+  getProjectTickets(id: number) {
+    this.http.get('https://freeapi.miniprojectideas.com/api/Jira/GetTicketsByProjectId?projectid=' + id).subscribe((res: any) => {
+      this.ticketsArray = res.data;
+    })
+  }
+
+  filterTicket(status: string) {
+    return this.ticketsArray.filter(m=>m.status == status)
+  }
+}
