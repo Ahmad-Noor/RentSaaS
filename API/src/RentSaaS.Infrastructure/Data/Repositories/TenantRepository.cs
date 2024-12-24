5f1d475eac31871eef7d3299d653f8ce8fca37e3
Ahmad Noor <ahmadnooreg@hotmail.com>
2024-12-24 13:26:57 -0600

    Add Tenant table and Controller

---
+++ API/src/RentSaaS.Infrastructure/Data/Repositories/TenantRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using RentSaaS.Domain.Entities;
+using RentSaaS.Domain.Interfaces.Repositories;
+
+namespace RentSaaS.Infrastructure.Data.Repositories;
+
+public class TenantRepository : Repository<Tenant>, ITenantRepository
+{
+    public TenantRepository(RentSaaSDBContext dbContext, ILogger logger) : base(dbContext, logger)
+    {
+    }
+}
