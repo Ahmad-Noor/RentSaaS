24986938827306d5c45e17b66387b306c588014b
Ahmad Noor <ahmadnooreg@hotmail.com>
2024-12-24 17:09:40 -0600

    // TenantController.cs

--- API/src/RentSaaS.API/Controllers/CoreControllers/TenantController.cs
+++ API/src/RentSaaS.API/Controllers/CoreControllers/TenantController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿// TenantController.cs
+using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
